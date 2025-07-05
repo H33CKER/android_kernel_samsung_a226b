ekuiter/torte-linux

ASoC: wm8962: Fix up incorrect error message in wm8962_set_fll
Use source instead of ret, which seems to be unrelated and will always
be zero.

Signed-off-by: Stuart Henderson <stuarth@opensource.cirrus.com>
Link: https://msgid.link/r/20240306161439.1385643-5-stuarth@opensource.cirrus.com
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/codecs/wm8962.c
@@ -2933,7 +2933,7 @@ static int wm8962_set_fll(struct snd_soc_component *component, int fll_id, int s
 				    WM8962_FLL_FRC_NCO, WM8962_FLL_FRC_NCO);
 		break;
 	default:
-		dev_err(component->dev, "Unknown FLL source %d\n", ret);
+		dev_err(component->dev, "Unknown FLL source %d\n", source);
 		return -EINVAL;
 	}
 

H33CKER/android_kernel_samsung_a226b

[ Upstream commit 96e202f8c52ac49452f83317cf3b34cd1ad81e18 ]
Signed-off-by: Sasha Levin <sashal@kernel.org>
(cherry picked from commit d619f175ff50582c05858cb96d05391f77c7c7aa)
[Harshit: Fix minor conflict due to missing commit: f4ee271709f0
("ASoC: wm8962: replace codec to component") in 4.14.y]
Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

sound/soc/codecs/wm8962.c
@@ -2866,7 +2866,7 @@ static int wm8962_set_fll(struct snd_soc_codec *codec, int fll_id, int source,
 				    WM8962_FLL_FRC_NCO, WM8962_FLL_FRC_NCO);
 		break;
 	default:
-		dev_err(codec->dev, "Unknown FLL source %d\n", ret);
+		dev_err(codec->dev, "Unknown FLL source %d\n", source);
 		return -EINVAL;
 	}
 