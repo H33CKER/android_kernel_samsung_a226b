4cb5e8bb8911ad008fe36571e2c597315c404935
H33CKER <gnanadan2010@outlook.com>
2025-07-06 10:18:11 +0100

    Newer build.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
+# Proton Clang path
+export PATH=$HOME/proton-clang/bin:$PATH
+
+# Toolchain and build settings
 export ARCH=arm64
+export SUBARCH=arm64
+export CC=clang
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 export ANDROID_MAJOR_VERSION=r
 
+# Optional flags
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a22x_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+# Output folder
+OUT_DIR=out
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Defconfig
+DEFCONFIG=a22x_defconfig
+
+# Start build
+make O=$OUT_DIR $DEFCONFIG
+make -j$(nproc) O=$OUT_DIR
+
+# Copy final kernel image
+cp $OUT_DIR/arch/arm64/boot/Image arch/arm64/boot/Image
